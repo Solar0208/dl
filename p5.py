5d143f917e911cb728d19d49915ea4c516c4ad6e
Solar0208 <aanchal@jagsoftware.in>
2026-04-14 10:30:11 +0530

    Update p5.py

--- p5.py
+++ p5.py
@@ -25,3 +25,80 @@ pipeline=Pipeline(estimators)
 kfold=KFold(n_splits=10)
 results=cross_val_score(pipeline,X,Y,cv=kfold)
 print("Wider: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+
+
+#multiclass Classification using KFold cross-validation
+
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+#loading dataset
+df=pandas.read_csv('Flower.csv',header=None)
+print(df)
+#splitting dataset into input and output variables
+X = df.iloc[:,0:4].astype(float)
+y=df.iloc[:,4]
+#print(X)
+#print(y)
+#encoding string output into numeric output
+encoder=LabelEncoder()
+encoder.fit(y)
+encoded_y=encoder.transform(y)
+print(encoded_y)
+dummy_Y=np_utils.to_categorical(encoded_y)
+print(dummy_Y)
+def baseline_model():
+# create model
+model = Sequential()
+model.add(Dense(8, input_dim=4, activation='relu'))
+model.add(Dense(3, activation='softmax'))
+# Compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+return model
+estimator=baseline_model()
+estimator.fit(X,dummy_Y,epochs=100,shuffle=True)
+action=estimator.predict(X)
+for i in range(25):
+ print(dummy_Y[i])
+print('^^^^^^^^^^^^^^^^^^^^^^')
+for i in range(25):
+ print(action[i])
+
+
+Code 2:
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+dataset=pandas.read_csv("Flower.csv",header=None)
+dataset1=dataset.values
+X=dataset1[:,0:4].astype(float)
+Y=dataset1[:,4]
+print(Y)
+encoder=LabelEncoder()
+encoder.fit(Y)
+encoder_Y=encoder.transform(Y)
+print(encoder_Y)
+dummy_Y=np_utils.to_categorical(encoder_Y)
+print(dummy_Y)
+def baseline_model():
+model=Sequential()
+model.add(Dense(8,input_dim=4,activation='relu'))
+model.add(Dense(3,activation='softmax'))
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+return model
+estimator=KerasClassifier(build_fn=baseline_model,epochs=100,batch_size=5)
+kfold = KFold(n_splits=10, shuffle=True)
+results = cross_val_score(estimator, X, dummy_Y, cv=kfold)
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+(Changing neuron)
+model.add(Dense(10,input_dim=4,activation='relu'))
